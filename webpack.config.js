cd23a284e9c79ddb7e62ef02c86e6ebc9d6d330a
vetalz <vitaly482@gmail.com>
2022-04-08 12:41:21 +0300

    webpack: added hash to name

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,19 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = {
   mode: 'production',
   entry: "./src/index.js",
   output: {
-    filename: "[name].js",
+    filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
     new HTMLWebpackPlugin({
       template: './src/index.html'
-    })
+    }),
+    new CleanWebpackPlugin()
   ],
   module: {
     rules: [
