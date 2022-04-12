ca601796e22aad7ca8d393f7c1383bc4747db180
vetalz <vitaly482@gmail.com>
2022-04-12 18:46:35 +0300

    getCardDeck moved into cardHelper

--- src/Game.js
+++ src/Game.js
@@ -1,48 +1,23 @@
-import Card from './Card';
 import Player from "./Player";
+import getCardDeck from "./cardHelper";
+
 class Game {
   players = [];
   currentPlayer;
   result = null;
-  cardDeck = [];
   endGame = false;
-  cardSuitVariant = ['diamond', 'heart', 'spade', 'club'];
-  cardValueVariant = [['2', 2], ['3', 3], ['4', 4], ['5', 5], ['6', 6], ['7', 7], ['8', 8], ['9', 9], ['10', 10],
-    ['J', 10], ['Q', 10], ['K', 10], ['A', 11]];
 
   constructor() {
     this.addPlayer('Alexander');
     this.addPlayer('Pavel');
     this.addPlayer('Victoria');
-    this.getCardDeck();
-    this.shuffle();
+    this.cardDeck = getCardDeck();
     this.startGiveCards();
   }
 
   addPlayer(name) {
     const id = this.players.length;
     this.players.push(new Player(id, name, []));
-  }
-
-  getCardDeck() {
-    for (const variant of this.cardSuitVariant) {
-      for (const value of this.cardValueVariant) {
-        this.cardDeck.push(new Card(variant, value[0], value[1]));
-      }
-    }
-  }
-
-  shuffle() {
-    let current = this.cardDeck.length;
-    let random;
-    while (current !== 0) {
-      random = Math.floor(Math.random() * current);
-      current--;
-
-      [this.cardDeck[current], this.cardDeck[random]] = [
-        this.cardDeck[random], this.cardDeck[current]
-      ];
-    }
   }
 
   startGiveCards() {
