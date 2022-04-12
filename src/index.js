8fe949bf1e5626dd5d554ea04c85459e861a4c45
vetalz <vitaly482@gmail.com>
2022-04-12 14:53:04 +0300

    remove innerHTML

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './index.scss';
 
 let game = new Game();
 
+let body = document.getElementById('body');
 let buttonHit = document.getElementById('hit');
 let buttonHit2 = document.getElementById('cardReverse');
 let buttonStand = document.getElementById('stand');
@@ -17,7 +18,7 @@ startRender();
 
 function startRender() {
   let divPlayers = document.getElementById('players');
-  divPlayers.innerHTML = '';
+  divPlayers.textContent = '';
   for (const player of game.players) {
     let elPlayer = document.createElement('div');
     let elCards = document.createElement('div');
@@ -29,32 +30,15 @@ function startRender() {
       game.currentPlayer = player;
     }
 
-    elCards.className = 'cards';
-    let profile = `<div class="profile">
-<!--               <img src="<%= require('./img/avatar_1.png') %>" alt="" class="avatar">-->
-                   <h2 class="name">${player.name}</h2>
-                 </div>`;
-    let score = `<div class="score">
-                 <span>${player.score}</span>
-               </div>`;
+    elPlayer.appendChild(createProfile(player));
+    elPlayer.appendChild(createScore(player));
 
-    elPlayer.innerHTML = profile + score;
+    elCards.className = 'cards';
 
     for (const card of player.cards) {
-      let elCard = document.createElement('div');
-      let color = getColor(card.cardSuit);
-      elCard.classList.add('card', color);
-      elCard.innerHTML = `<div class="top">
-                          <span>${card.cardName}</span>
-                        </div>
-                        <div class="middle">
-                          <span>${renderSuit(card.cardSuit)}</span>
-                        </div>
-                        <div class="bottom">
-                          <span>${card.cardName}</span>
-                        </div>`;
-      elCards.appendChild(elCard);
+      elCards.appendChild(createCard(card));
     }
+
     elPlayer.appendChild(elCards);
     divPlayers.appendChild(elPlayer);
   }
@@ -91,21 +75,11 @@ function addCard(player) {
   const elPlayer = document.getElementById(player.id);
   const elCards = document.querySelector(`[id='${player.id}'] > .cards`);
   const elScore = document.querySelector(`[id='${player.id}'] > .score > span`);
-  const elCard = document.createElement('div');
+
   const card = player.cards[player.cards.length-1];
-  const color = getColor(card.cardSuit);
-  elCard.classList.add('card', color);
-  elCard.innerHTML = `<div class="top">
-                          <span>${card.cardName}</span>
-                        </div>
-                        <div class="middle">
-                          <span>${renderSuit(card.cardSuit)}</span>
-                        </div>
-                        <div class="bottom">
-                          <span>${card.cardName}</span>
-                        </div>`;
-  elCards.appendChild(elCard);
-  elScore.innerHTML = player.score;
+  elCards.appendChild(createCard(card))
+
+  elScore.textContent = player.score;
   if (player.isOver) {
     elScore.classList.add('red');
   }
@@ -130,23 +104,7 @@ function nextPlayer() {
 }
 
 function renderModalBlock() {
-  let body = document.getElementById('body');
-  let modal = document.createElement('div');
-  modal.classList.add('modal-back');
-  modal.id = 'modal';
-  modal.innerHTML = `<div class="modal">
-                        <h2>Result</h2>
-                        <h1>
-                            <span class="black">♠</span>
-                            <span class="red">♥</span>
-                            ${game.result}
-                            <span class="red">♦</span>
-                            <span class="black">♣</span>
-                        </h1>
-                        <button class="hit" id="again">Play again</button>
-                    </div>`
-  body.appendChild(modal);
-
+  body.appendChild(createModal());
   let elButton = document.getElementById('modal');
   elButton.addEventListener('click', removeModal);
 }
@@ -156,4 +114,103 @@ function removeModal() {
   elModal.remove();
   game = new Game();
   startRender();
+}
+
+function createProfile(player) {
+  const profile = document.createElement('div');
+  const h2 = document.createElement('h2')
+
+  profile.classList.add('profile');
+  h2.classList.add('name');
+  h2.textContent = player.name;
+
+  profile.appendChild(h2);
+
+  return profile;
+}
+
+function createScore(player) {
+  const score = document.createElement('div');
+  const span = document.createElement('span');
+
+  score.classList.add('score');
+  span.textContent = player.score;
+
+  score.appendChild(span);
+
+  return score;
+}
+
+function createCard(card) {
+  const elCard = document.createElement('div');
+  const top = document.createElement('div');
+  const span1 = document.createElement('span');
+  const middle = document.createElement('div');
+  const span2 = document.createElement('span');
+  const bottom = document.createElement('div');
+  const span3 = document.createElement('span');
+  const color = getColor(card.cardSuit);
+
+  elCard.classList.add('card', color);
+  top.classList.add('top');
+  middle.classList.add('middle');
+  bottom.classList.add('bottom');
+
+  span1.textContent = card.cardName;
+  span2.textContent = renderSuit(card.cardSuit);
+  span3.textContent = card.cardName;
+
+  top.appendChild(span1);
+  middle.appendChild(span2);
+  bottom.appendChild(span3);
+
+  elCard.appendChild(top);
+  elCard.appendChild(middle);
+  elCard.appendChild(bottom);
+
+  return elCard;
+}
+
+function createModal() {
+  const modalBack = document.createElement('div');
+  const modal = document.createElement('div');
+  const h2 = document.createElement('h2');
+  const h1 = document.createElement('h1');
+  const span1 = document.createElement('span');
+  const span2 = document.createElement('span');
+  const span3 = document.createElement('span');
+  const span4 = document.createElement('span');
+  const span5 = document.createElement('span');
+  const button = document.createElement('button');
+
+  modalBack.classList.add('modal-back');
+  modal.classList.add('modal');
+  span1.classList.add('black');
+  span2.classList.add('red');
+  span4.classList.add('red');
+  span5.classList.add('black');
+  button.classList.add('hit');
+
+  modalBack.id = 'modal';
+  button.id = 'again';
+
+  h2.textContent = 'Result';
+  span1.textContent = '♠';
+  span2.textContent = '♥';
+  span3.textContent = game.result;
+  span4.textContent = '♦';
+  span5.textContent = '♣';
+  button.textContent = 'Play again';
+
+  const spans = [span1, span2, span3, span4, span5];
+  for (const item of spans) {
+    h1.appendChild(item);
+  }
+
+  modal.appendChild(h2);
+  modal.appendChild(h1);
+  modal.appendChild(button);
+  modalBack.appendChild(modal);
+
+  return modalBack;
 }
